4d6662cff4718a4ced5e700187c59246d1225719
EnZzzzzz <957078942@qq.com>
2023-09-04 01:11:02 +0800

    添加分形循环

--- doc/an_introduction_to_shader_art_coding.glsl
+++ doc/an_introduction_to_shader_art_coding.glsl
@@ -11,25 +11,25 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord){
     vec2 uv0  = uv;
     vec3 finalColor = vec3(0.);
 
-    uv = fract(uv * 2.0) - 0.5;
-
-    float d = length(uv);
-
-   
     vec3 col_a = vec3(0.5, 0.5, 0.5);
     vec3 col_b = vec3(0.5, 0.5, 0.5);
     vec3 col_c = vec3(1.0, 1.0, 1.0);
     vec3 col_d = vec3(0.263, 0.416, 0.557);
 
-    vec3 col = palette(length(uv0) + iTime, col_a, col_b, col_c, col_d);
+    for (float i=0.0; i < 3.0; i++){
+        uv = fract(uv * 1.5) - 0.5;
 
-    d = sin(d * 8.0 + iTime) / 8.0;
-    d = abs(d);
+        float d = length(uv);
 
-    d = 0.02 / d;
+        vec3 col = palette(length(uv0) + iTime * 0.4, col_a, col_b, col_c, col_d);
 
-    col *= d;
-    finalColor += col * d;
+        d = sin(d * 8.0 + iTime) / 8.0;
+        d = abs(d);
 
+        d = 0.02 / d;
+
+        col *= d;
+        finalColor += col * d;
+    }
     fragColor = vec4(finalColor, 1.0);
 }
